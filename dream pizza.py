e7ff701ce11022163db7166d8774746d42e9a479
Connor Dobson <dobscon2@kvc.school.nz>
2017-08-29 03:58:45 +0000

    Update dream pizza.py

--- dream pizza.py
+++ dream pizza.py
@@ -4,20 +4,20 @@ delivery_charge = 3
 customer_order = []
 customer_price = []
 
-if user_delivery == "yes":
+if user_delivery.lower() == "yes":
   user_address = input("What is your address?")
   user_number = input("What is your phone number?")
   customer_price.append(delivery_charge)
 else:
   del customer_price[:]
-
+  
 while True:
   pizza_menu_regular = ["Ham & Cheese", "Hawaiian", "Meatlovers", "Pepperoni", "Vegetarian", "Cheesy"]
   pizza_menu_gourmet = ["Chicken & Cranberry", "BBQ Chicken & Bacon", "Butter Chicken Pizza", "Seafood Deluxe", "Chicken Satay Pizza", "Smoked Salmon Pizza"]
   regular_pizza = 8.50
   gourmet_pizza = 13.50
   user_input = input("""What would you like to order?
- Regular Pizzas
+Regular Pizzas
  1) Ham & Cheese - $8.50
  2) Hawaiian - $8.50
  3) Meatlovers - $8.50
@@ -68,12 +68,12 @@ while True:
   if user_input == "12":
     customer_order.append(pizza_menu_gourmet[5])
     customer_price.append(gourmet_pizza)
-  if user_input == "end":
+  if user_input.lower() == "end":
     break
   else:
     print("Please choose one of the available pizzas")
 
-if user_delivery == "yes":
+if user_delivery.lower() == "yes":
   print(user_name)
   print("Delivery")
   print(user_address)
@@ -81,8 +81,8 @@ if user_delivery == "yes":
   print(customer_order)
   print("Total Price: ${:.2f}".format(sum(customer_price)))
  
-if user_delivery == "no":
+if user_delivery.lower() == "no":
   print(user_name)
   print("Pick Up")
   print(customer_order)
-  print("Total Price: ${:.2f)".format(sum(customer_price)))
+  print("Total Price: ${:.2f}".format(sum(customer_price)))
